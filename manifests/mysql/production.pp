e3095d7fdd6ff1c03d92e5d595d21564f8f07115
Aron Butler <abutler@legal-suite.com>
2018-02-27 17:41:35 -0500

    Initial database configuration.

---
+++ manifests/mysql/production.pp
@@ -0,0 +1,26 @@
+class profile::mysql {
+    class { '::mysql::server':
+      root_password           => '*3929D05C6D847F751BA50F59C4F6BC69A74EAF83',
+      remove_default_accounts => true,
+      override_options        => $override_options
+
+    }
+}
+
+class profile::mysql::production inherits profile::mysql {
+  
+  mysql::db { 'prod':
+    user     => 'clientprod',
+    password => 'mypass',
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE'],
+  }
+  
+  mysql::db { 'qa':
+    user     => 'clientqa',
+    password => 'mypass',
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE'],
+  }
+
+}
