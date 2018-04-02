de35c2790b1bba8eec27e232f3c2fa38ea5f883b
Aaron Butler <abutler@legal-suite.com>
2018-04-02 15:33:50 -0400

    Adding tomcat 8.5 instances

--- manifests/tomcat/production.pp
+++ manifests/tomcat/production.pp
@@ -1,6 +1,31 @@
 class profile::tomcat::production {
 
-    include tomcat
+  class { 'java': }
+
+  tomcat::install { '/opt/tomcat8':
+    source_url => 'https://www.apache.org/dist/tomcat/tomcat-8/v8.5.29/bin/apache-tomcat-8.5.29.tar.gz'
+  }
+  tomcat::instance { 'tomcat8Prod':
+    catalina_home => '/opt/tomcat8',
+    catalina_base => '/opt/tomcat8Prod',
+  }
+  tomcat::instance { 'tomcat8QA':
+    catalina_home => '/opt/tomcat8',
+    catalina_base => '/opt/tomcat8QA',
+  }
+  # Change the default port of the second instance server and HTTP connector
+  tomcat::config::server { 'tomcat8QA':
+    catalina_base => '/opt/tomcat8QA',
+    port          => '8006',
+  }
+  tomcat::config::server::connector { 'tomcat8QA-http':
+    catalina_base         => '/opt/tomcat8QA',
+    port                  => '8081',
+    protocol              => 'HTTP/1.1',
+    additional_attributes => {
+      'redirectPort' => '8443'
+    },
+  }
     
 
 }
