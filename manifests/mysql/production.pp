6e5ed88c46d6364ffefda2d770568c4cc5fabfe0
Aaron Butler <abutler@legal-suite.com>
2018-04-03 11:54:51 -0400

    Added MariaDB installation and default client DB and user creation.

--- manifests/mysql/production.pp
+++ manifests/mysql/production.pp
@@ -1,27 +1,43 @@
 class profile::mysql {
-    class { '::mysql::server':
-      root_password           => '*3929D05C6D847F751BA50F59C4F6BC69A74EAF83',
-      remove_default_accounts => true,
-      service_enabled         => true,
-      override_options        => $override_options,
+    
+    class {'::mysql::server':
+      package_name     => 'mariadb-server',
+      service_name     => 'mysql',
+      root_password    => '*3929D05C6D847F751BA50F59C4F6BC69A74EAF83',
+      override_options => {
+        mysqld => {
+          'log-error' => '/var/log/mysql/mariadb.log',
+          'pid-file'  => '/var/run/mysqld/mysqld.pid',
+        },
+        mysqld_safe => {
+          'log-error' => '/var/log/mysql/mariadb.log',
+        },
+      }
+    }
 
+    class {'::mysql::client':
+      package_name    => 'mariadb-client',
+      bindings_enable => true,
     }
 }
 
 class profile::mysql::production inherits profile::mysql {
   
-  mysql::db { 'prod':
-    user     => 'clientprod',
-    password => 'mypass',
+  mysql::db { "${facts['networking']['hostname']}prod":
+    user     => "${facts['networking']['hostname']}prod",
+    password => "${facts['networking']['hostname']}2018LS",
     host     => 'localhost',
     grant    => ['SELECT', 'UPDATE'],
   }
   
-  mysql::db { 'qa':
-    user     => 'clientqa',
-    password => 'mypass',
+  mysql::db { "${facts['networking']['hostname']}qa":
+    user     => "${facts['networking']['hostname']}qa",
+    password => "${facts['networking']['hostname']}qa2018LS",
     host     => 'localhost',
     grant    => ['SELECT', 'UPDATE'],
   }
 
 }
+
+
+
