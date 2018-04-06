d5ba8206de439723e0f6ba3d03fa2796b0549a1f
Aaron Butler <abutler@legal-suite.com>
2018-04-06 14:36:36 -0400

    Adding Fail2Ban configurations

--- manifests/fail2ban/production.pp
+++ manifests/fail2ban/production.pp
@@ -1,8 +1,15 @@
-class profile::fail2ban::production  {
-
+class profile::fail2ban::production (
+    String $publicips,
+  ) {
+  
+  $ipcommand = "sed -i '/ignoreip = 127.0.0.1\/8*/c\ignoreip = 127.0.0.1\/8 ${publicips}.' /etc/fail2ban/jail.conf"
+  
   package { 'fail2ban':
     ensure => installed,
   }
-  
-  
+
+  exec { 'publicips':
+    command => $ipcommand,
+    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/games', '/usr/local/games'],
+  }
 }
