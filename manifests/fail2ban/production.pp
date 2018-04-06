0425ac5bffef52f6a99f7ea2cf99d08191118355
Aaron Butler <abutler@legal-suite.com>
2018-04-06 17:34:57 -0400

    Added fail2ban config

--- manifests/fail2ban/production.pp
+++ manifests/fail2ban/production.pp
@@ -1,15 +1,33 @@
 class profile::fail2ban::production (
     String $publicips,
   ) {
-  
-  $ipcommand = "sed -i '/ignoreip = 127.0.0.1\/8*/c\ignoreip = 127.0.0.1\/8 ${publicips}.' /etc/fail2ban/jail.conf"
-  
+    
   package { 'fail2ban':
     ensure => installed,
   }
+  file { "/etc/fail2ban/jail.conf": # file destination
+    content => epp('profile/fail2ban/jail.conf.epp', {}),
+    owner    => root,
+    group    => root,
+    mode     => '0644',
+  }
+  
+  file { "/etc/fail2ban/filter.d/modsec.conf": # file destination
+    content => epp('profile/fail2ban/modsec.conf.epp', {}),
+    owner    => root,
+    group    => root,
+    mode     => '0644',
+  }
 
-  exec { 'publicips':
-    command => $ipcommand,
-    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/games', '/usr/local/games'],
+  file_line { 'publicips':
+    ensure => present,
+    path   => '/etc/fail2ban/jail.conf',
+    line   => "ignoreip = 127.0.0.1/8 ${publicips}",
+    match  => 'ignoreip = 127.0.0.1/8',
+  } 
+  
+  service { 'fail2ban':
+    ensure => running,
+    enable => true,
   }
 }
