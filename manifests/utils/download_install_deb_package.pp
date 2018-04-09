a1765385fec1978db0e90fc78fe4e20e16dc75d1
Aaron Butler <abutler@legal-suite.com>
2018-04-09 14:00:22 -0400

    Made the download and install deb package idempotent.

--- manifests/utils/download_install_deb_package.pp
+++ manifests/utils/download_install_deb_package.pp
@@ -10,27 +10,27 @@ define profile::utils::download_install_deb_package (
     $package_real = $title
   }
 
-  $package_path = "/tmp/${package_real}"
-
-  exec {'download':
-    command => "/usr/bin/wget -O ${package_path} ${url}"
+  $package_path = "/opt/${package_real}"
+  exec { 'download':
+    command  => "wget ${url}",
+    path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    cwd      => '/opt',
+    user     => 'root',
+    creates  => "${package_path}",
+    notify   => Exec['install']
   }
 
   exec {'install':
     command => "dpkg -i ${package_path}",
-    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/games', '/usr/local/games'],
+    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    refreshonly  => true,
+    notify   => Exec['install2']
   }
 
   exec {'install2':
     command => "sudo apt-get install -f",
-    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/games', '/usr/local/games'],
+    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    refreshonly  => true,
   }
-
-  file {'cleanup':
-    ensure => absent,
-    path   => "${package_path}",
-  }
-
-  Exec['download'] -> Exec['install'] -> Exec['install2'] -> File['cleanup']
 
 }
